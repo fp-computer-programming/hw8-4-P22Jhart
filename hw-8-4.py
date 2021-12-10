e863309923680009eb4671ea98a5fb9f4eb8a166
Jordan Hart <p22jhart@fairfieldprep.org>
2021-12-09 21:57:44 -0500

    hw
    s

---
+++ hw-8-4.py
@@ -0,0 +1,14 @@
+import random
+
+randum_number = random.randint(0,50)
+
+geuss = int(input("Geuss a number betwwenn 0-50"))
+
+if (randum_number < geuss):
+
+    print("to high")
+
+if geuss < randum_number:
+    print("too low")
+    elif:
+        print("your number")
